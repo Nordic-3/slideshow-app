7d5511046a4e12fc4c6cf222ddd29eaf260db894
Barát Marcell <barimarci1@gmail.com>
2026-02-02 09:53:27 +0100

    REF: get images number

--- src/app/services/images/images.service.ts
+++ src/app/services/images/images.service.ts
@@ -12,8 +12,11 @@ export class ImagesService {
 
   collectionName = 'images';
 
-     imageNumber(): Observable<number[]> {
-    return this.afs.collection<{ imageNumber : number }>(this.collectionName, ref => ref.where('id', '==', 'Ta2iWGJUrKrZ6iylAp24'))
-    .valueChanges().pipe(map(docs => docs.map(doc => doc.imageNumber)));
-  }
+    imageNumber(): Observable<number> {
+  return this.afs.collection<{ imageNumber: number }>(this.collectionName, ref => ref.limit(1))
+    .valueChanges()
+    .pipe(
+      map(docs => docs.length > 0 ? docs[0].imageNumber : 0)
+    );
+}
 }
